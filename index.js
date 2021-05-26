7d636ecf37d60356af310dbe60704fb0eff8e8a5
Glenn Goossens <glgoose@pm.me>
2021-05-26 16:44:17 +0200

    adds reddit as source

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const randomButton = document.querySelector('button.random')
 
-async function getRandomDadjoke () {
+// set & select source
+let source = 'icanhaz'
+
+const sourceEl = document.getElementById('source')
+
+sourceEl.addEventListener('change', e => (source = e.target.value))
+
+// get random dadjoke
+async function fetchFromIcanHaz () {
   const res = await fetch('https://icanhazdadjoke.com/', {
     headers: {
       Accept: 'application/json'
@@ -10,11 +18,31 @@ async function getRandomDadjoke () {
   return data.joke
 }
 
+async function fetchFromReddit () {
+  const res = await fetch('https://www.reddit.com/r/dadjokes.json')
+  const data = await res.json()
+  // debugger
+  const jokes = data.data.children
+  const randomNum = Math.floor(Math.random() * jokes.length + 1)
+  const joke =
+    jokes[randomNum].data.title + '<br><br>' + jokes[randomNum].data.selftext
+  return joke
+}
+
+async function getRandomDadjoke () {
+  if (source === 'icanhaz') {
+    return await fetchFromIcanHaz()
+  }
+  if (source === 'reddit') {
+    return await fetchFromReddit()
+  }
+}
+
 async function displayDadjoke () {
   const dadjoke = await getRandomDadjoke()
 
   const p = document.querySelector('.dadjoke')
-  p.innerText = dadjoke
+  p.innerHTML = dadjoke
 }
 
 randomButton.addEventListener('click', displayDadjoke)
