fe9532b68b64b88d45afd46e95fb18cef1d29b57
Glenn Goossens <glgoose@pm.me>
2021-05-26 14:27:25 +0200

    adds random dad joke button

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-const searchInput = document.querySelector('.search .input')
-const searchButton = document.querySelector('.search .button')
+const randomButton = document.querySelector('button.random')
+
+async function getRandomDadjoke () {
+  const res = await fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      Accept: 'application/json'
+    }
+  })
+  const data = await res.json()
+  return data.joke
+}
+
+async function displayDadjoke () {
+  const dadjoke = await getRandomDadjoke()
+
+  const p = document.querySelector('.dadjoke')
+  p.innerText = dadjoke
+}
+
+randomButton.addEventListener('click', displayDadjoke)
+
+// const searchInput = document.querySelector('.search .input')
+// const searchButton = document.querySelector('.search .button')
+// function searchDadjoke (e) {
+//   const input = e.target.value
+// }
+
+// searchInput.addEventListener('input', searchDadjoke)
