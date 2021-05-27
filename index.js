62a455d416451619bf990570f1c98081540feeeb
Glenn Goossens <glgoose@pm.me>
2021-05-27 15:23:51 +0200

    rm console.log

--- index.js
+++ index.js
@@ -3,9 +3,38 @@ const randomButton = document.querySelector('button.random')
 // set & select source
 let source = 'icanhaz'
 
-const sourceEl = document.getElementById('source')
+// const sourceEl = document.getElementById('source')
 
-sourceEl.addEventListener('change', e => (source = e.target.value))
+// sourceEl.addEventListener('change', e => (source = e.target.value))
+
+const toggleDropdown = () => dropdown.classList.toggle('is-active')
+
+// const closeDropdown = () => {
+//   console.log('close')
+//   dropdown.classList.remove('is-active')
+// }
+
+const dropdown = document.querySelector('.dropdown')
+const dropdownButton = dropdown.querySelector('.button')
+
+dropdownButton.addEventListener('click', toggleDropdown)
+dropdown.addEventListener('blur', toggleDropdown)
+
+const dropdownItems = [...document.getElementsByClassName('dropdown-item')]
+dropdownItems.forEach(dropdownItem =>
+  dropdownItem.addEventListener('click', e => {
+    const el = e.target
+    if (!el.classList.contains('is-active')) {
+      dropdownItems.forEach(item => item.classList.remove('is-active'))
+      el.classList.add('is-active')
+    }
+    source = el.innerText
+    const selectedEl = document.querySelector('.dropdown-selected')
+    selectedEl.innerText = source
+
+    toggleDropdown()
+  })
+)
 
 // get random dadjoke
 async function fetchFromIcanHaz () {
@@ -21,7 +50,6 @@ async function fetchFromIcanHaz () {
 async function fetchFromReddit () {
   const res = await fetch('https://www.reddit.com/r/dadjokes.json')
   const data = await res.json()
-  // debugger
   const jokes = data.data.children
   const randomNum = Math.floor(Math.random() * jokes.length + 1)
   const joke =
